9866a70ef4e839b275b1b943646c3439cc047aa9
Kay Wood <11405108+kwood15@users.noreply.github.com>
2018-11-30 17:29:15 +0000

    Refactor components

--- src/components/hotel/HotelList.js
+++ src/components/hotel/HotelList.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import Sort from '../sort';
-import Filter from '../filter';
 import HotelItem from './HotelItem';
 
 import { HotelsWrapper, HotelsList } from './HotelsStyles';
-import { FiltersWrapper, FiltersList } from '../filter/FiltersStyles';
-
-// import hotels from '../../data/hotels';
+import { FiltersWrapper, FiltersList, FiltersListItem } from '../filter/FiltersStyles';
 
 class HotelList extends Component {
-  state = {
-    hotels: [],
-    starRatingValue: 0
+  constructor(props) {
+  super(props)
+    this.state = {
+      hotels: [],
+      starRatingValue: 0,
+      filterKey: 'BY_ACTIVE',
+      filterValue: ''
+    }
   }
 
   componentDidMount() {
@@ -21,15 +21,23 @@ class HotelList extends Component {
 
   loadData() {
     fetch('/data/hotels.json')
-    .then(response => response.json())
-    .then(data => this.saveData(data))
-    .catch(error => console.error(error));
+      .then(response => response.json())
+      .then(data => this.saveData(data))
+      .catch(error => console.error(error));
   }
 
   saveData(data) {
     this.setState({
       hotels: data
     })
+  }
+
+  handleClick = (e) => {
+    this.setState({
+      filterKey: 'BY_FACILITY',
+      filterValue: e.target.value
+    });
+    console.log(this.state.filterValue);
   }
 
   handleChange = event => {
@@ -41,53 +49,46 @@ class HotelList extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    alert('star rating' + this.state.starRatingValue);
+    alert(`star rating: ${this.state.starRatingValue}`);
   }
 
   renderData(hotels) {
     const { starRatingValue } = this.state;
-    const { filterKey } = this.props;
+    return (
+      <HotelsWrapper className="search-results hotels">
+        <h1>Search results for ...</h1>
 
-    const HOTEL_FILTERS = {
-      'BY_ACTIVE': hotels.map(x => x.name),
-      'BY_FACILITY': hotels.map(x => x.facilities.map(y => y))
-    };
-
-    const filteredHotels = hotels.filter(hotel => HOTEL_FILTERS[filterKey]);
-
-    if (hotels && hotels.length) {
-      return (
-        <HotelsWrapper className="search-results hotels">
-          <h1>Search results for ...</h1>
-
-          <FiltersWrapper className="hotels__filters">
-            <FiltersList>
-              {hotels.map(hotel => (
-                <Filter key={hotel.name} {...hotel} />
-              ))}
-            </FiltersList>
-          </FiltersWrapper>
-
-          <div className="hotels__sort-by">
-            <form onSubmit={this.handleSubmit}>
-              <label>Sort by:</label>
-              <select value={starRatingValue} onChange={this.handleChange}>
-                {hotels.map(hotel => (
-                  <option value={hotel.starRating}>{hotel.starRating}</option>
+        <FiltersWrapper className="hotels__filters">
+          <FiltersList>
+            {hotels.map(hotel => (
+              <FiltersListItem key={hotel.name}>
+                {hotel.facilities.map((facility, index) => (
+                  <button key={index} onClick={this.handleClick} value={facility}>{facility}</button>
                 ))}
-              </select>
-              <input type="submit" value="Submit" />
-            </form>
-          </div>
-
-          <HotelsList className="hotels__list">
-            {filteredHotels.map(hotel => (
-              <HotelItem key={hotel.name} {...hotel} />
+              </FiltersListItem>
             ))}
-          </HotelsList>
-        </HotelsWrapper>
-      );
-    }
+          </FiltersList>
+        </FiltersWrapper>
+
+        <div className="hotels__sort-by">
+          <form onSubmit={this.handleSubmit}>
+            <label>Sort by:</label>
+            <select value={starRatingValue} onChange={this.handleChange}>
+              {hotels.map(hotel => (
+                <option key={hotel.name} value={hotel.starRating}>{hotel.starRating}</option>
+              ))}
+            </select>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+
+        <HotelsList className="hotels__list">
+          {hotels.map(hotel => (
+            <HotelItem key={hotel.name} {...hotel} />
+          ))}
+        </HotelsList>
+      </HotelsWrapper>
+    );
   }
 
   renderLoading() {
@@ -95,7 +96,15 @@ class HotelList extends Component {
   }
 
   render() {
-    const { hotels } = this.state;
+    const { hotels, filterKey } = this.state;
+
+    const HOTEL_FILTERS = {
+      'BY_ACTIVE': hotels.map(hotel => hotel.name),
+      'BY_FACILITY': hotels.map(hotel => hotel.facilities)
+    };
+
+    const filteredHotels = HOTEL_FILTERS[filterKey];
+    console.log(filteredHotels)
 
     return (
       hotels && hotels.length ? this.renderData(hotels) : this.renderLoading()
--- src/components/sort/index.js
+++
@@ -1,9 +0,0 @@
-import React, { Fragment } from 'react';
-
-const Sort = ({ starRatingValue, starRating }) => (
-  <Fragment>
-    <option value={starRatingValue}>{starRating}</option>
-  </Fragment>
-);
-
-export default Sort;
